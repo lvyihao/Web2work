bedc2e7de3dbd2d3476b097cabae88937b3d314f
lvyihao <1026315613@qq.com>
2019-01-06 17:50:40 +0800

    lvyihao

---
+++ js/xiangqing.js
@@ -0,0 +1,13 @@
+// 顶部悬浮
+// window.onload = function(){
+// 	var cover = document.getElementsByClassName('cover')[0];
+// 	window.onscroll = function(){
+// 		var st = document.documentElement.scrollTop || document.body.scrollTop ;
+// 		if(st>168){
+// 			cover.style.position = 'fixed'
+// 		}else{
+// 			cover.style.position = 'static'
+// 		}
+// 	}
+// }
+
